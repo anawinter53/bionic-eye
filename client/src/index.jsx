f14eaf401dd7167d2eecdf0998c02ad09aa9eff8
anawinter53 <anatolia.winter@yahoo.com>
2023-04-13 13:04:25 +0100

    add more files

---
+++ client/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+// import store from './store'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+)
